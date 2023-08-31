278f1b037feff01b47af7352deb40d105c62a567
kprl884 <kprl884@gmail.com>
2023-08-31 10:08:43 +0300

    Edited ui at BasketScreen

--- app/src/main/java/com/example/shoplistapp/components/productitem/ProductItem.kt
+++ app/src/main/java/com/example/shoplistapp/components/productitem/ProductItem.kt
@@ -1,24 +1,92 @@
 package com.example.shoplistapp.components.productitem
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.shoplistapp.domain.entity.ProductItem
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ProductItem(productItem: ProductItem) {
-    Row {
-        Text(text = productItem.title)
-        Text(text = productItem.category)
-        Text(text = productItem.description)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            GlideImage(
+                model = productItem.image,
+                contentDescription = "Item image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = productItem.title,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Description: $${productItem.description}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = "Price: $${productItem.price}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(
+                    onClick = {//onSubtract}
+                    },
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(80.dp)
+                ) {
+                    Text(text = "Subtract")
+                }
+            }
+        }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewProductItem() {
-   ProductItem(
-        ProductItem(3, "Şapka", 500f, "Şapka", "Şapka",
-        "")
+    ProductItem(
+        ProductItem(
+            3, "Şapka", 500f, "Şapka", "Şapka",
+            ""
+        )
     )
 }
--- app/src/main/java/com/example/shoplistapp/presentation/basket/BasketScreen.kt
+++ app/src/main/java/com/example/shoplistapp/presentation/basket/BasketScreen.kt
@@ -1,17 +1,11 @@
 package com.example.shoplistapp.presentation.basket
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoplistapp.components.productitem.ProductItem
 import com.example.shoplistapp.ui.theme.ShoplistAppTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,33 +15,11 @@ fun BasketScreen(
     uiStateFlow: StateFlow<BasketScreenUiState>,
     onEvent: (BasketUiEvent) -> Unit
 ) {
-    var item by remember {
-        mutableIntStateOf(0)
-    }
-    val localList = MutableStateFlow(BasketScreenUiState())
-    //val localList: StateFlow<ShoppingScreenUiState> = _localList.asStateFlow()
-
     Column {
-        Text(text = "Add", modifier = Modifier
-            .clickable {
-                item += 1
-
-            }
-        )
-        Text(text = "Delete", modifier = Modifier
-            .clickable {
-                item -= 1
-
-            })
-        Text(text = "Get List", modifier = Modifier
-            .clickable {
-                localList.value.basketList = uiStateFlow.value.basketList
-            })
-
         LazyColumn {
-            items(localList.value.basketList) {
+            items(uiStateFlow.value.basketList) {
                 if (it != null) {
-                    Text(text = it.title)
+                    ProductItem(it)
                 }
             }
         }
